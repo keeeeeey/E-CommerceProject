d5730ebd68960e2168832bc701d15f8f5ee233fb
keeeeeey <sseioul@naver.com>
2022-04-29 08:45:49 +0900

    [init] 마켓보로 과제

---
+++ src/main/java/com/example/marketboro/MarketboroApplication.java
@@ -0,0 +1,13 @@
+package com.example.marketboro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketboroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarketboroApplication.class, args);
+    }
+
+}
