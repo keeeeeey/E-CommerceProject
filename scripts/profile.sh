c60bf563c733be2c59ef5c3a42f4c90bd2604b76
keeeeeey <sseioul@naver.com>
2022-06-03 05:13:32 +0900

    [FIX]

--- scripts/profile.sh
+++ scripts/profile.sh
@@ -5,8 +5,6 @@ function find_idle_profile()
     # curl 결과로 연결할 서비스 결정
     RESPONSE_CODE=$(curl -s -o /dev/null -w "%{http_code}" http://localhost/profile)
 
-
-
     if [ ${RESPONSE_CODE} -ge 400 ] # 400 보다 크면 (즉, 40x/50x 에러 모두 포함)
     then
         CURRENT_PROFILE=real2
