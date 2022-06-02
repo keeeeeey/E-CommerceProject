7ecce7fce93553e311cb5864627835853d840742
keeeeeey <sseioul@naver.com>
2022-06-02 10:50:45 +0900

    [FIX]

--- src/test/java/com/example/marketboro/repository/CartProductRepositoryTest.java
+++ src/test/java/com/example/marketboro/repository/CartProductRepositoryTest.java
@@ -7,16 +7,17 @@ import com.example.marketboro.repository.cart.CartRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 
 import java.util.List;
-import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class CartProductRepositoryTest {
 
     @Autowired
