db156b3b49d5919bf09040e724f098700ec282b8
keeeeeey <sseioul@naver.com>
2022-06-04 01:04:36 +0900

    [FIX]

--- scripts/start.sh
+++ scripts/start.sh
@@ -13,10 +13,10 @@ PROJECT_NAME=springboot-intro
 echo "> Build 파일 복사"
 echo "> cp $REPOSITORY/deploy/*.jar $REPOSITORY/"
 
-cp $REPOSITORY/deloy/*.jar $REPOSITORY/
+cp $REPOSITORY/deploy/*.jar $REPOSITORY/
 
 echo "> 새 어플리케이션 배포"
-JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
+JAR_NAME=$(ls -tr $REPOSITORY/deploy/*.jar | tail -n 1)
 
 echo "> JAR Name: $JAR_NAME"
 
@@ -32,4 +32,4 @@ echo "> $JAR_NAME 를 profile=$IDLE_PROFILE 로 실행합니다."
 nohup java -jar \
     -Dspring.config.location=classpath:/application-$IDLE_PROFILE.yml \
     -Dspring.profiles.active=$IDLE_PROFILE
-    $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
+    $JAR_NAME > $REPOSITORY/deploy/nohup.out 2>&1 &
