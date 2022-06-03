0e9c97911a7082ec43eb6b6d692f9dcde320daab
keeeeeey <sseioul@naver.com>
2022-06-04 01:22:50 +0900

    [FIX]

--- scripts/start.sh
+++ scripts/start.sh
@@ -7,13 +7,8 @@ ABSPATH=$(readlink -f $0)
 ABSDIR=$(dirname $ABSPATH)
 source ${ABSDIR}/profile.sh
 
-REPOSITORY=/home/ubuntu/app
-PROJECT_NAME=springboot-intro
-
-echo "> Build 파일 복사"
-echo "> cp $REPOSITORY/deploy/*.jar $REPOSITORY/"
-
-cp $REPOSITORY/deploy/*.jar $REPOSITORY/
+REPOSITORY=/home/ec2-user/dplus
+PROJECT_NAME=code-0.0.1-SNAPSHOT
 
 echo "> 새 어플리케이션 배포"
 JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
@@ -31,5 +26,4 @@ IDLE_PROFILE=$(find_idle_profile)
 echo "> $JAR_NAME 를 profile=$IDLE_PROFILE 로 실행합니다."
 nohup java -jar \
     -Dspring.config.location=classpath:/application-$IDLE_PROFILE.yml \
-    -Dspring.profiles.active=$IDLE_PROFILE
-    $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
+    -Dspring.profiles.active=$IDLE_PROFILE $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
