797ddf9e413ce035ac00a6a449ee285ca318cd56
keeeeeey <sseioul@naver.com>
2022-06-04 13:01:52 +0900

    [FIT]

--- scripts/start.sh
+++ scripts/start.sh
@@ -10,11 +10,6 @@ source ${ABSDIR}/profile.sh
 REPOSITORY=/home/ubuntu/market
 PROJECT_NAME=code-0.0.1-SNAPSHOT
 
-echo "> Build 파일 복사"
-echo "> cp $REPOSITORY/*.jar $REPOSITORY/"
-
-cp $REPOSITORY/*.jar $REPOSITORY/
-
 echo "> 새 어플리케이션 배포"
 JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
 
@@ -31,5 +26,4 @@ IDLE_PROFILE=$(find_idle_profile)
 echo "> $JAR_NAME 를 profile=$IDLE_PROFILE 로 실행합니다."
 nohup java -jar \
     -Dspring.config.location=classpath:/application-$IDLE_PROFILE.yml \
-    -Dspring.profiles.active=$IDLE_PROFILE
-    $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
+    -Dspring.profiles.active=$IDLE_PROFILE $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
