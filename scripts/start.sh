005ef436a8c3d649d3b944cbf5cdb533e48e63ae
keeeeeey <sseioul@naver.com>
2022-06-04 12:26:34 +0900

    [FIX]

--- scripts/start.sh
+++ scripts/start.sh
@@ -11,9 +11,9 @@ REPOSITORY=/home/ec2-user/market
 PROJECT_NAME=code-0.0.1-SNAPSHOT
 
 echo "> Build 파일 복사"
-echo "> cp $REPOSITORY/*.jar $REPOSITORY/"
+echo "> cp $REPOSITORY/deploy/*.jar $REPOSITORY/"
 
-cp $REPOSITORY/*.jar $REPOSITORY/
+cp $REPOSITORY/deploy/*.jar $REPOSITORY/
 
 echo "> 새 어플리케이션 배포"
 JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
@@ -31,4 +31,5 @@ IDLE_PROFILE=$(find_idle_profile)
 echo "> $JAR_NAME 를 profile=$IDLE_PROFILE 로 실행합니다."
 nohup java -jar \
     -Dspring.config.location=classpath:/application-$IDLE_PROFILE.yml \
-    -Dspring.profiles.active=$IDLE_PROFILE $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
+    -Dspring.profiles.active=$IDLE_PROFILE
+    $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
